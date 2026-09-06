17b7ce7ef083e445836e263c279352198abb15ed
Fk-Dev-05 <fkhanxx69@gmail.com>
2026-09-06 13:33:39 +0530

    Added project files

---
+++ Hertzsoft/some js/nav.js
@@ -0,0 +1,8 @@
+$(window).on("scroll", function() {
+    if ($(window).scrollTop() > 0) {
+      $(".navbar").addClass("scrolled");
+    } else {
+      $(".navbar").removeClass("scrolled");
+    }
+  });
+  
---
+++ Hertzsoft/some js/script.js
@@ -0,0 +1,3 @@
+function msg(){
+    alert("Thankyou For Registering We Will Be Back Soon");
+}
